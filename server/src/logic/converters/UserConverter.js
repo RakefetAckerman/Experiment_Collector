f9edd293543519f07256c876383625aa03a82309
ShovalShabi <shovalshabi@gmail.com>
2023-09-04 09:23:16 +0300

    Documented user converter

--- server/src/logic/converters/UserConverter.js
+++ server/src/logic/converters/UserConverter.js
@@ -1,53 +1,78 @@
-import UserBoundary from "../../boundaries/user/UserBoundary.js"
-import UserModel from "../../models/UserModel.js"
-import Roles from "../../utils/UserRole.js"
-import createHttpError from "http-errors";
+import UserBoundary from "../../boundaries/user/UserBoundary.js"; // Import UserBoundary class
+import UserModel from "../../models/UserModel.js"; // Import UserModel class
+import Roles from "../../utils/UserRole.js"; // Import Roles enumeration
+import createHttpError from "http-errors"; // Import createHttpError for HTTP error handling
 
+// Define an object for user conversion operations
 const userConverter = {
+    /**
+     * Converts a UserModel instance to a UserBoundary instance.
+     *
+     * @param {UserModel} userModel - The UserModel instance to convert.
+     * @returns {UserBoundary} The converted UserBoundary instance.
+     * @throws {createHttpError.BadRequest} If an invalid user role is encountered.
+     */
     toBoundary: (userModel) => {
+        // Extract the user's role from the UserModel
         const role = userModel.role;
-    
-        console.log(Object.values(Roles));
 
-        if (!Object.values(Roles).includes(userModel.role))
+        // Check if the role is a valid role defined in the Roles enum
+        if (!Object.values(Roles).includes(userModel.role)) {
             throw new createHttpError.BadRequest("Invalid user role");
+        }
 
+        // Create a new UserBoundary using data from UserModel
         const userBoundary = new UserBoundary(
             userModel.userId.platform,
-            userModel.userId.email,
+            userModel.userId.outerIdentifier,
             userModel.role,
             userModel.username,
             userModel.userDetails
         );
 
-        console.log("Converted user to boundary");
-
+        // Return the UserBoundary instance
         return userBoundary;
     },
+
+    /**
+     * Converts a UserBoundary instance to a UserModel instance.
+     *
+     * @param {UserBoundary} userBoundary - The UserBoundary instance to convert.
+     * @returns {UserModel} The converted UserModel instance.
+     * @throws {createHttpError.BadRequest} If an invalid user role is encountered.
+     */
     toModel: (userBoundary) => {
+        // Extract the role from UserBoundary
         const role = userBoundary.role;
+
+        // Create an invertedRoles object to map role names to enum values
         const invertedRoles = Object.fromEntries(
             Object.entries(Roles).map(([key, value]) => [value, key])
         );
-        const mappedRole = Roles[invertedRoles[role]]; // Get the mapped role value from the Roles enum
 
+        // Get the mapped role value from the Roles enum using the invertedRoles object
+        const mappedRole = Roles[invertedRoles[role]];
+
+        // Check if the mapped role is valid
         if (!mappedRole) {
             throw new createHttpError.BadRequest("Invalid user role");
         }
 
+        // Create a new UserModel using data from UserBoundary
         const userModel = new UserModel({
             userId: {
                 platform: userBoundary.userId.platform,
-                email: userBoundary.userId.email
+                email: userBoundary.userId.outerIdentifier
             },
             role: mappedRole, // Use the mapped role value
             username: userBoundary.username,
             userDetails: userBoundary.userDetails
         });
 
-        console.log("Converted user to model");
-
+        // Return the UserModel instance
         return userModel;
     }
 };
+
+// Export the userConverter object for use in other modules
 export default userConverter;
