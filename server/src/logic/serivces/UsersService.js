7e1ef859cdd2340030f3101420f388272ccf285e
ShovalShabi <shovalshabi@gmail.com>
2023-09-05 13:46:50 +0300

    Wrotw login and register methods for user serivce

--- server/src/logic/serivces/UsersService.js
+++ server/src/logic/serivces/UsersService.js
@@ -1,31 +1,78 @@
+import mongoose from "mongoose";
 import UserBoundary from "../../boundaries/user/UserBoundary.js";
 import UserModel from "../../models/UserModel.js";
 import userConverter from "../converters/UserConverter.js";
 import createHttpError from 'http-errors';
+import Roles from "../../utils/UserRole.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const { Error } = mongoose;
 
 const userService = {
 
-  createUser: async (newUser) => {
-    const existingUser = await UserModel.findOne({ 'userId.email': newUser.email });
+  createUser: async (reqUserBoundary) => {
+    console.log("In create, ", reqUserBoundary);
+    const existingUser = await UserModel.findOne({
+      'userId': reqUserBoundary.userId.email + "$" + reqUserBoundary.userId.platform
+    });
 
-    if (existingUser)
-      throw new createHttpError.BadRequest("User already exists")
+    console.log("In create the model is, ", existingUser);
 
-    const userModel = userConverter.toModel(new UserBoundary(
-      newUser.platform,
-      newUser.email,
-      newUser.role,
-      newUser.username,
-      newUser.userDetails));
+    if (existingUser) {
+      return userService.login(reqUserBoundary);
+    }
 
-    userModel.save()
+    const userModel = userConverter.toModel(reqUserBoundary);
 
-    return userConverter.toBoundary(userModel);
+    console.log("In create the model is, ", existingUser);
+
+    return userModel.validate()
+      .then(async () => {
+        /*In case the user is an Admin or Researcher the client will send thier password within the UserDetails */
+        if ( userModel.role !== Roles.PARTICIPANT){
+          const salt = await bcrypt.genSalt();
+          if (!userModel.userDetails.hasOwnProperty('password')) {
+            throw new Error.ValidationError();
+          }
+          userModel.userDetails.password = await bcrypt.hash(userModel.userDetails.password, salt);
+        }
+      })
+      .then(() => userModel.save())
+      .catch((error) => {
+        if (error instanceof Error.ValidationError) {
+          throw new createHttpError.BadRequest("Invalid input, some of the fields for creating new user are missing");
+        }
+        throw error;
+      })
+      .then(() => userConverter.toBoundary(userModel));
   },
 
-  login: (userSuperApp, userEmail) => {
-    console.log("Trying to login");
-    // Implement logic here
+  login: async (reqUserBoundary) => {
+    const existingUserModel = await UserModel.findOne({
+      'userId': reqUserBoundary.userId.email + "$" + reqUserBoundary.userId.platform
+    });
+
+    console.log("userModel--> ", existingUserModel);
+
+    /* In case that none particpant with special authrizations tries to log in wihout signup first
+    * The Client will have two seperate logins, one for authorized users with special premissions such
+    * as Admin and Reseacher, which there users will have to go through signup and then login, the Particpants
+    * in other case will have to go everytimy by signup, if they are exist the server will return them 
+    */
+    if (!existingUserModel && reqUserBoundary.role !== Roles.PARTICIPANT)
+      throw new createHttpError.NotFound("User does not exists");
+
+    if (existingUserModel.role !== Roles.PARTICIPANT) {
+      const isMatch = await bcrypt.compare(reqUserBoundary.userDetails.password, existingUserModel.userDetails.password);
+      if (!isMatch)
+        throw new createHttpError.BadRequest("Invalid credentials");
+
+      const token = jwt.sign({ id: existingUserModel._id }, process.env.JWT_SECRET);
+      const userBoundary = userConverter.toBoundary(existingUserModel);
+      delete userBoundary.userDetails.password;
+      return { token, userBoundary };
+    }
+    return userConverter.toBoundary(existingUserModel);
   },
 
   updateUser: (userSuperApp, userEmail, update) => {
