c2445bfa411d38b133d63dbb49bf79e3b3478b5f
ShovalShabi <shovalshabi@gmail.com>
2023-09-10 20:59:58 +0300

    Added object routes for authorized users

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,9 +2,12 @@
 import express from "express";
 import { verifyToken } from "../logic/middleware/auth.js";
 import userRoutes from "./users.js";
+import objectRoutes from "./objects.js";
 
 const router = express.Router();
 
 router.use("/users", verifyToken, userRoutes);//Letting the auth router know the users' router
+router.use("/objects", verifyToken, objectRoutes);//Letting the auth router know the objects' router
+
 
 export default router;
---
+++ server/src/routes/objects.js
@@ -0,0 +1,134 @@
+import express from "express";
+import userService from "../logic/serivces/UsersService.js";
+import UserBoundary from "../boundaries/user/UserBoundary.js";
+import ObjectBoundary from "../boundaries/object/ObjectBoundary.js";
+import objectsService from "../logic/serivces/ObjectsService.js";
+
+
+const router = express.Router();
+
+/**
+ * Route for creating new object
+ * @name POST objects/
+ * @function
+ * @param {Object} req - Express request object formed as UserBoundary.
+ * @param {Object} res - Express response object.
+ * @returns {Object<ObjectBoundary>} JSON response as UserBoundary structure containing user details.
+ * @throws {import("http-errors").HttpError} JSON response containing Http error message.
+ */
+router.post("/", async (req, res) => {
+  const objectData = req.body; // Getting the body of the request containing the NewUserBoundary data
+  try {
+    const reqObjectBoundary = new ObjectBoundary(
+      undefined,
+      objectData.type,
+      objectData.alias,
+      objectData.active,
+      undefined,
+      undefined,
+      objectData.location,
+      objectData.createdBy,
+      objectData.objectDetails);
+
+    const resUserBoundary = await objectsService.createObject(reqObjectBoundary);
+    res.status(201).json(resUserBoundary);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message }); // Use "error.status" instead of "error.code"
+  }
+});
+
+/**
+ * Route for user login.
+ * @name POST users/login
+ * @function
+ * @param {Object} req - Express request object formed as UserBoundary.
+ * @param {Object} res - Express response object.
+ * @returns {Object<UserBoundary>} JSON response of token and UserBoundary structure for the
+ * user details in case the user is not Particpant, otherwise there will be no JWT token.
+ * @throws {import("http-errors").HttpError} JSON response containing Http error message.
+ */
+router.post("/login", async (req, res) => {
+  const userData = req.body; // Getting the body of the request containing the NewUserBoundary data
+  try {
+    const reqUserBoundary = new UserBoundary(
+      userData.platform,
+      userData.email,
+      userData.role,
+      userData.username,
+      userData.userDetails);
+    const DBResponse = await userService.login(reqUserBoundary);
+    res.status(200).json(DBResponse);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message }); // Use "error.status" instead of "error.code"
+  }
+});
+
+/**
+ * Route for updating user information.
+ * @name PUT users/:email/:platform
+ * @function
+ * @param {Object} req - Express request object formed as UserBoundary.
+ * @param {Object} res - Express response object.
+ * @returns {Object} An empty JSON reposne.
+ * @throws {import("http-errors").HttpError} JSON response containing Http error message.
+ */
+router.put("/:email/:platform", async (req, res) => {
+  const userEmail = req.params.email;
+  const userPlatform = req.params.platform;
+  const userData = req.body; // Getting the body of the request containing the NewUserBoundary data
+  try {
+    const reqUserBoundary = new UserBoundary(
+      userData.platform,
+      userData.email,
+      userData.role,
+      userData.username,
+      userData.userDetails);
+    const DBResponse = await userService.updateUser(userEmail,userPlatform, reqUserBoundary);
+    res.status(200).json(DBResponse);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message }); // Use "error.status" instead of "error.code"
+  }
+});
+
+/**
+ * Route for getting user information.
+ * @name GET users/:email/:platform
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {[Object]} An Array of JSON object structured as UserBoundary form.
+ * @throws {import("http-errors").HttpError} JSON response containing Http error message.
+ */
+router.get("/:email/:platform", async (req, res) => {
+  const userEmail = req.params.email;
+  const userPlatform = req.params.platform;
+  try {
+    const DBResponse = await userService.getAllUsers(userEmail,userPlatform);
+    res.status(200).json(DBResponse);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message }); // Use "error.status" instead of "error.code"
+  }
+});
+
+/**
+ * Route for deleting all users (only accessible to Admins).
+ * @name DELETE users/:email/:platform
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Object} JSON response containing deletion status.
+ * @throws {import("http-errors").HttpError} JSON response containing Http error message.
+ */
+router.delete("/:email/:platform", async (req, res) => {
+  const userEmail = req.params.email;
+  const userPlatform = req.params.platform;
+  try {
+    const DBResponse = await userService.deleteAllUsers(userEmail,userPlatform);
+    res.status(200).json(DBResponse);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message }); // Use "error.status" instead of "error.code"
+  }
+});
+
+
+export default router;
