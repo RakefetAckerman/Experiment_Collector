1612b18e161582b8c8647ebc2a3dabdbeb852446
ShovalShabi <shovalshabi@gmail.com>
2023-09-10 05:32:55 +0300

    Finished users service for now, also documented the code

--- server/src/logic/serivces/UsersService.js
+++ server/src/logic/serivces/UsersService.js
@@ -8,8 +8,18 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const { Error } = mongoose;
 
+/**
+ * @description User Service handles user-related operations like creating, updating, and deleting users.
+ */
 const userService = {
-
+  /**
+   * Creates a new user.
+   * @async
+   * @function
+   * @param {UserBoundary} reqUserBoundary - The user details to create a new user.
+   * @returns {Promise<UserBoundary>} The created user details after saving it within the database.
+   * @throws {Error} Throws an error if the user creation process encounters any issues.
+   */
   createUser: async (reqUserBoundary) => {
     const existingUser = await UserModel.findOne({
       'userId': reqUserBoundary.userId.email + "$" + reqUserBoundary.userId.platform
@@ -41,7 +51,14 @@ const userService = {
       })
       .then(() => userConverter.toBoundary(userModel));
   },
-
+  /**
+   * Logs in a user.
+   * @async
+   * @function
+   * @param {UserBoundary} reqUserBoundary - The user details for login.
+   * @returns {Promise<{ token: string, userBoundary: UserBoundary }>} The JWT token and user details.
+   * @throws {Error} Throws an error if the login process encounters any issues.
+   */
   login: async (reqUserBoundary) => {
     const existingUserModel = await UserModel.findOne({
       'userId': reqUserBoundary.userId.email + "$" + reqUserBoundary.userId.platform
@@ -60,14 +77,23 @@ const userService = {
       if (!isMatch)
         throw new createHttpError.BadRequest("Invalid credentials");
 
-      const token = jwt.sign({ id: existingUserModel._id }, process.env.JWT_SECRET, { expiresIn: 9999999 });
+      const token = jwt.sign({ id: existingUserModel._id }, process.env.JWT_SECRET, { expiresIn: 99999 });
       const userBoundary = userConverter.toBoundary(existingUserModel);
       delete userBoundary.userDetails.password;
       return { token, userBoundary };
     }
     return userConverter.toBoundary(existingUserModel);
   },
-
+  /**
+   * Updates a user's information.
+   * @async
+   * @function
+   * @param {string} userEmail - The email of the user.
+   * @param {string} userPlatform - The platform of the user.
+   * @param {UserBoundary} updateUser - The user details to update.
+   * @returns {Promise<UserBoundary>} The updated user details.
+   * @throws {Error} Throws an error if the update process encounters any issues.
+   */
   updateUser: async (userEmail, userPlatform, updateUser) => {
     const existingUserModel = await UserModel.findOne({
       'userId': userEmail + "$" + userPlatform
@@ -85,21 +111,66 @@ const userService = {
         const salt = await bcrypt.genSalt();
         additionalDetails.password = await bcrypt.hash(additionalDetails.password, salt);
       }
-      existingUserModel.userDetails = {...additionalDetails};
+      existingUserModel.userDetails = {
+        ...existingUserModel.userDetails,
+        ...additionalDetails
+      };
     }
     existingUserModel.save();
     return userConverter.toBoundary(existingUserModel);
   },
+  /**
+   * Gets all users (only accessible to Admins).
+   * @async
+   * @function
+   * @param {string} userEmail - The email of the user making the request.
+   * @param {string} userPlatform - The platform of the user making the request.
+   * @returns {Promise<UserModel[]>} An array of user models.
+   * @throws {Error} Throws an error if the request encounters any issues.
+   */
+  getAllUsers: async (userEmail, userPlatform) => {
+    const existingUserModel = await UserModel.findOne({
+      'userId': userEmail + "$" + userPlatform
+    });
 
-  getAllUsers: (userSuperApp, userEmail, size, page) => {
-    console.log("Getting all UserBoundary objects");
-    // Implement logic here
+    if (!existingUserModel)
+      throw new createHttpError.NotFound("User does not exists");
+
+    if (existingUserModel.role === Roles.ADMIN) {
+      const usersArr = await UserModel.find();
+      return usersArr;
+    }
+    else
+      throw new createHttpError.Forbidden("You are not allowed to make this request");
   },
+  /**
+   * Deletes all users (only accessible to Admins).
+   * @async
+   * @function
+   * @param {string} userEmail - The email of the user making the request.
+   * @param {string} userPlatform - The platform of the user making the request.
+   * @returns {Promise<{ n: number, deletedCount: number, ok: number }>} Deletion status.
+   * @throws {Error} Throws an error if the request encounters any issues.
+   */
+  deleteAllUsers: async (userEmail, userPlatform) => {
+    const existingUserModel = await UserModel.findOne({
+      'userId': userEmail + "$" + userPlatform
+    });
 
-  deleteAllUsers: (userSuperApp, userEmail) => {
-    console.log("Deleting all UserBoundary objects");
-    // Implement logic here
+    if (!existingUserModel)
+      throw new createHttpError.NotFound("User does not exists");
+
+    if (existingUserModel.role === Roles.ADMIN) {
+      const usersArr = await UserModel.deleteMany();
+      return usersArr;
+    }
+    else
+      throw new createHttpError.Forbidden("You are not allowed to make this request");
   }
 };
 
+/**
+ * Exporting the userService object for further use by other modules if needed.
+ * @type {Object}
+ */
 export default userService;
