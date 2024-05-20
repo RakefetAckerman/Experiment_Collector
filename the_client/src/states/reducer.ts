c41e5e820b182fe2a4a18d64dcc862712a59d3ed
ShovalShabi <shovalshabi@gmail.com>
2024-05-20 19:12:16 +0300

    Configured redux reducer for handling the states by actions

--- the_client/src/states/reducer.ts
+++ the_client/src/states/reducer.ts
@@ -8,28 +8,37 @@ interface State {
   user: UserBoundaryImpl | null;
   token: string | null;
   mode: "light" | "dark";
+  expiry: string | null;
   userRole: UserRoles;
   lastEditedItem: ObjectBoundaryImpl | null;
   lastAnswer: ObjectBoundaryImpl | null;
 }
 
+export type { State };
+
 const initialState: State = {
   user: null,
   token: null,
   mode: "light",
+  expiry: null,
   userRole: UserRoles.Participant,
   lastEditedItem: null,
   lastAnswer: null,
 };
 
 // Define actions
-const logedIn = createAction<{ user: UserBoundaryImpl; token: string }>(
-  ReduxActions.LOGED_IN
-);
+const logedIn = createAction<{
+  user: UserBoundaryImpl;
+  token: string;
+  expiry: string;
+}>(ReduxActions.LOGED_IN);
+
 const logedOut = createAction<void>(ReduxActions.LOGED_OUT);
+
 const answeredItem = createAction<{ lastAnswer: ObjectBoundaryImpl }>(
   ReduxActions.ANSWERED_ITEM
 );
+
 const editedItem = createAction<{ lastEditedItem: ObjectBoundaryImpl }>(
   ReduxActions.EDITED_ITEM
 );
@@ -42,10 +51,33 @@ const reducer = createReducer(initialState, (builder) => {
       deepCopyUser.userDetails = { ...action.payload.user.userDetails };
       state.user = deepCopyUser;
       state.token = action.payload.token;
+      state.expiry = action.payload.expiry;
+      let role;
+      switch (deepCopyUser.role) {
+        case UserRoles.Admin:
+          role = UserRoles.Admin;
+          break;
+
+        case UserRoles.Researcher:
+          role = UserRoles.Researcher;
+          break;
+
+        case UserRoles.Participant:
+          role = UserRoles.Participant;
+          break;
+
+        default:
+          role = UserRoles.Participant;
+          break;
+      }
+      state.userRole = role;
+      return state;
     })
     .addCase(logedOut, (state) => {
       state.user = null;
       state.token = null;
+      state.expiry = null;
+      return state;
     })
     .addCase(answeredItem, (state, action) => {
       const deepCopyAnswer = { ...action.payload.lastAnswer };
@@ -53,6 +85,7 @@ const reducer = createReducer(initialState, (builder) => {
         ...action.payload.lastAnswer.objectDetails,
       };
       state.lastAnswer = deepCopyAnswer;
+      return state;
     })
     .addCase(editedItem, (state, action) => {
       const deepCopyEditedItem = { ...action.payload.lastEditedItem };
@@ -60,7 +93,10 @@ const reducer = createReducer(initialState, (builder) => {
         ...action.payload.lastEditedItem.objectDetails,
       };
       state.lastEditedItem = action.payload.lastEditedItem;
+      return state;
     });
 });
 
 export default reducer;
+
+export { logedIn, logedOut, answeredItem, editedItem };
