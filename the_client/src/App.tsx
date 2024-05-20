527ea317155c647c66085bd0c6e4312df904e6fd
ShovalShabi <shovalshabi@gmail.com>
2024-05-20 19:10:22 +0300

    Added Redux configurations for state mangement and also applied basic routing conditions

--- the_client/src/App.tsx
+++ the_client/src/App.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import HomePage from "./scenes/homePage/HomePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
-import { AuthState } from "./states";
-import ParticipantsLoginClassifier from "./components/LoginClassifier";
+import LoginClassifier from "./components/LoginClassifier";
 import SignupPrep from "./components/SignupPrep";
+import { State } from "./states/reducer";
+import DashboardPage from "./scenes/dashboardPage/DashboardPage";
 
 function App() {
-  const mode = useSelector((state: AuthState) => state.mode);
+  const mode = useSelector((state: State) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state: State) => state.token));
 
   return (
     <>
@@ -19,11 +21,19 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/home" element={<HomePage />} />
             <Route
-              path="/login/:userType"
-              element={<ParticipantsLoginClassifier />}
+              path="/"
+              element={
+                isAuth ? <HomePage /> : <Navigate to="/login/Researcher" />
+              }
             />
+            <Route
+              path="/Dashboard"
+              element={
+                isAuth ? <DashboardPage /> : <Navigate to="/login/Researcher" />
+              }
+            />
+            <Route path="/login/:userType" element={<LoginClassifier />} />
             <Route path="/signup" element={<SignupPrep />} />
           </Routes>
         </ThemeProvider>
