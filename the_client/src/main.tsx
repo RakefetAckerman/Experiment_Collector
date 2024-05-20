ba6736bca827e847cdee6aced7cfacbaa6eb710d
ShovalShabi <shovalshabi@gmail.com>
2024-05-20 19:10:56 +0300

    Added Redux state mangement for the entire application

--- the_client/src/main.tsx
+++ the_client/src/main.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { store } from "./states/index.ts";
 import { Provider } from "react-redux";
+import store, { persistor } from "./states/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-    ,
   </React.StrictMode>
 );
