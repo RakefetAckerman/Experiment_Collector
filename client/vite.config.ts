c4d92e5e8a7c8c5c0adbcf9d1f38a107dbd9f015
ShovalShabi <shovalshabi@gmail.com>
2024-06-12 20:14:01 +0300

    made adjustments for the containerized application, added scripts for each environments

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from "dotenv";// Import the dotenv-flow package for loading environment variables
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv"; // Import the dotenv-flow package for loading environment variables
 
 // Load environment variables from .env files
 dotenv.config();
@@ -9,6 +9,6 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.VITE_PORT || 3000), // Use the PORT environment variable or default to 3000
+    port: Number(process.env.VITE_PORT || 3001), // Use the PORT environment variable or default to 3001
   },
-})
+});
