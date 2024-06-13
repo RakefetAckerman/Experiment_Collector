4006afff62e81d698c5af1c3b0add4be715e2fd7
ShovalShabi <shovalshabi@gmail.com>
2024-06-13 19:36:46 +0300

    Altered the usersSerivce specification

--- client/src/services/usersService.ts
+++ client/src/services/usersService.ts
@@ -14,36 +14,46 @@ interface UsersService {
 
   /**
    * Logs in an existing user.
-   * @param existingUserBoundary The user data to log in.
+   * @param email The email of the user.
+   * @param platform The platform of the user.
    * @returns A Promise resolving to an object containing user body.
    * @throws {Error} If login fails due to incorrect credentials or other errors.
    */
-  login(existingUserBoundary: UserBoundary): Promise<UserBoundary>;
+  login(email: string, platform: string): Promise<UserBoundary>;
 
   /**
    * Updates an existing user.
+   * @param email The email of the user.
+   * @param platform The platform of the user.
    * @param userToUpdate The updated user data.
    * @returns A Promise as void.
    * @throws {Error} If there is an error during the user update process.
    */
-  updateUser(userToUpdate: UserBoundary): Promise<void>;
+  updateUser(
+    email: string,
+    platform: string,
+    userToUpdate: UserBoundary
+  ): Promise<void>;
 
   /**
    * Retrieves all users based on specified criteria.
-   * @param existingUserBoundary The user data.
+   * @param email The email of the user.
+   * @param platform The platform of the user.
    * @returns A Promise resolving to an array of UserBoundary objects.
    * @throws {Error} If there is an error retrieving the users.
    */
-  getAllUsers(existingUserBoundary: UserBoundary): Promise<UserBoundary[]>;
+  getAllUsers(email: string, platform: string): Promise<UserBoundary[]>;
 
   /**
    * Deletes all users based on specified criteria.
-   * @param existingUserBoundary The user data.
+   * @param email The email of the user.
+   * @param platform The platform of the user.
    * @returns A Promise resolving to an object containing deletion statistics.
    * @throws {Error} If there is an error deleting the users.
    */
   deleteAllUsers(
-    existingUserBoundary: UserBoundary
+    email: string,
+    platform: string
   ): Promise<{ n: number; deletedCount: number; ok: number }>;
 }
 
