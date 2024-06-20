316201bff46e5e81093ccaefeb0b76c247e160f4
ShovalShabi <shovalshabi@gmail.com>
2024-06-20 10:38:15 +0300

    Added more actions to redux store, now redux can fetch the next question randomly or fixed position and also saving the questions of each experiment in storage

--- client/src/states/reducer.ts
+++ client/src/states/reducer.ts
@@ -12,6 +12,9 @@ interface State {
   userRole: UserRoles;
   lastEditedItem: ObjectBoundaryImpl | null;
   lastAnswer: ObjectBoundaryImpl | null;
+  currentExperiment: ObjectBoundaryImpl | null;
+  nextQuestion: ObjectBoundaryImpl | null;
+  questionsOfCurrentExp: ObjectBoundaryImpl[] | null;
 }
 
 export type { State };
@@ -24,6 +27,9 @@ const initialState: State = {
   userRole: UserRoles.Participant,
   lastEditedItem: null,
   lastAnswer: null,
+  currentExperiment: null,
+  nextQuestion: null,
+  questionsOfCurrentExp: null,
 };
 
 // Define actions
@@ -43,7 +49,29 @@ const editedItem = createAction<{ lastEditedItem: ObjectBoundaryImpl }>(
   ReduxActions.EDITED_ITEM
 );
 
-// Create reducer
+const setCurrentExperiment = createAction<{
+  currentExperiment: ObjectBoundaryImpl;
+}>(ReduxActions.SET_CURRENT_EXPERIMENT);
+
+const unsetCurrentExperiment = createAction<void>(
+  ReduxActions.UNSET_CURRENT_EXPERIMENT
+);
+
+const prepareNextQuestion = createAction<void>(
+  ReduxActions.PEREPARE_NEXT_QUESTION
+);
+
+const loadQuestionsToExp = createAction<{ questions: ObjectBoundaryImpl[] }>(
+  ReduxActions.LOAD_QUESTIONS_TO_EXP
+);
+
+// Type guards
+const isArrayOfBooleans = (value: unknown): value is boolean[] =>
+  Array.isArray(value) && value.every((item) => typeof item === "boolean");
+
+const isArrayOfStrings = (value: unknown): value is string[] =>
+  Array.isArray(value) && value.every((item) => typeof item === "string");
+
 const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(logedIn, (state, action) => {
@@ -57,15 +85,12 @@ const reducer = createReducer(initialState, (builder) => {
         case UserRoles.Admin:
           role = UserRoles.Admin;
           break;
-
         case UserRoles.Researcher:
           role = UserRoles.Researcher;
           break;
-
         case UserRoles.Participant:
           role = UserRoles.Participant;
           break;
-
         default:
           role = UserRoles.Participant;
           break;
@@ -94,9 +119,80 @@ const reducer = createReducer(initialState, (builder) => {
       };
       state.lastEditedItem = action.payload.lastEditedItem;
       return state;
+    })
+    .addCase(setCurrentExperiment, (state, action) => {
+      const deepCopyCurrentExperiment = { ...action.payload.currentExperiment };
+      deepCopyCurrentExperiment.objectDetails = {
+        ...action.payload.currentExperiment.objectDetails,
+      };
+      state.currentExperiment = deepCopyCurrentExperiment;
+      return state;
+    })
+    .addCase(unsetCurrentExperiment, (state) => {
+      state.currentExperiment = null;
+      state.questionsOfCurrentExp = null;
+    })
+    .addCase(prepareNextQuestion, (state) => {
+      if (state.currentExperiment && state.questionsOfCurrentExp) {
+        const currentExp = state.currentExperiment;
+        const experimentFlow = currentExp.objectDetails.experimentFlow;
+
+        if (isArrayOfBooleans(experimentFlow) && experimentFlow.length > 0) {
+          const indicator = experimentFlow.shift()!; //Getting the indicator of the question if exists
+
+          // true - the question is fixed and here is a need to disaply it by order
+          if (indicator) {
+            const fixedQuestions = currentExp.objectDetails.fixedQuestions; // Refrencing the array
+            if (isArrayOfStrings(fixedQuestions) && fixedQuestions.length > 0) {
+              const quesIternalID = fixedQuestions.shift()!; // Retrieving the id of the first order of the fixed quetions
+              const nextQuestion = state.questionsOfCurrentExp.find(
+                (child) => child.objectId.internalObjectId === quesIternalID
+              ); //Getting the child object that has the target id
+              state.nextQuestion = nextQuestion || null;
+            }
+            // false - the question is randomized
+          } else {
+            const randomizedQuestions =
+              currentExp.objectDetails.randomizedQuestions; // Refrencing the array
+            if (
+              isArrayOfStrings(randomizedQuestions) &&
+              randomizedQuestions.length > 0
+            ) {
+              const randomIndex = Math.floor(
+                Math.random() * randomizedQuestions.length
+              );
+              const quesIternalID = randomizedQuestions.splice(
+                randomIndex,
+                1
+              )[0]; // Choosing a random ID by index and reorganizing the array
+
+              const nextQuestion = state.questionsOfCurrentExp.find(
+                (child) => child.objectId.internalObjectId === quesIternalID
+              ); // Fetching the actual question object
+
+              state.nextQuestion = nextQuestion || null; // Setting the next question
+            }
+          }
+        } else {
+          state.nextQuestion = null;
+        }
+      }
+    })
+    .addCase(loadQuestionsToExp, (state, action) => {
+      state.questionsOfCurrentExp = [...action.payload.questions];
+      return state;
     });
 });
 
 export default reducer;
 
-export { logedIn, logedOut, answeredItem, editedItem };
+export {
+  logedIn,
+  logedOut,
+  answeredItem,
+  editedItem,
+  setCurrentExperiment,
+  unsetCurrentExperiment,
+  prepareNextQuestion,
+  loadQuestionsToExp,
+};
