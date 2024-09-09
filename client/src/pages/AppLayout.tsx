b0b182b8e1a9f6a67aa57a7b512c2dad8c1be93d
Ofir Mikel <159753ofir@gmail.com>
2024-09-09 14:47:18 +0300

    Phone Navbar -Ofir: fixed bug with bg-color

--- client/src/pages/AppLayout.tsx
+++ client/src/pages/AppLayout.tsx
@@ -2,14 +2,13 @@ import {Outlet} from "react-router-dom";
 import SideBar from "../components/sideBar/SideBar.tsx";
 import useViewportWidth from "../hooks/useViewWidth.ts";
 import {TABLET_SIZE} from "../utils/constants.ts";
-import NavBarPhone from "../components/common/NavBarPhone.tsx";
+import NavBarPhone from "../components/sideBar/NavBarPhone.tsx";
 
 function AppLayout() {
     const width = useViewportWidth()
-    console.log(width)
     return (
         <div className="flex max-tablet:flex-col tablet:flex-row ">
-            {width < TABLET_SIZE ?<NavBarPhone></NavBarPhone> :<SideBar/>}
+            {width < TABLET_SIZE ?<NavBarPhone/> :<SideBar/>}
             <div className="w-full h-dvh bg-background-grey">
                 <Outlet/>
             </div>
